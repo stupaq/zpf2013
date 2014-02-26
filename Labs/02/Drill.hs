2aa34103f9b11beede10461cf535ca97a19a3644
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-02-26 18:35:35 +0100

    lab02/short tasks

--- Labs/02/Drill.hs
+++ Labs/02/Drill.hs
@@ -1,34 +1,40 @@
+module Drill where
+
 class Fluffy f where
   furry :: (a -> b) -> f a -> f b
- 
+  -- It really looks like Functor
+
 -- Exercise 1
 -- Relative Difficulty: 1
 instance Fluffy [] where
-  furry = error "todo"
- 
+  furry = map
+
 -- Exercise 2
 -- Relative Difficulty: 1
 instance Fluffy Maybe where
-  furry = error "todo"
- 
+  furry = fmap
+
 -- Exercise 3
 -- Relative Difficulty: 5
 instance Fluffy ((->) t) where
-  furry = error "todo"
- 
+  furry = (.)
+  -- We could easily put fmap here, just like above
+
 newtype EitherLeft b a = EitherLeft (Either a b)
 newtype EitherRight a b = EitherRight (Either a b)
- 
+
 -- Exercise 4
 -- Relative Difficulty: 5
 instance Fluffy (EitherLeft t) where
-  furry = error "todo"
- 
+  furry f (EitherLeft (Left x)) = EitherLeft $ Left $ f x
+  furry _ (EitherLeft (Right x)) = EitherLeft $ Right x
+
 -- Exercise 5
 -- Relative Difficulty: 5
 instance Fluffy (EitherRight t) where
-  furry = error "todo"
- 
+  furry f (EitherRight (Right x)) = EitherRight $ Right $ f x
+  furry _ (EitherRight (Left x)) = EitherRight $ Left x
+
 class Misty m where
   banana :: (a -> m b) -> m a -> m b
   unicorn :: a -> m a
@@ -36,88 +42,91 @@ class Misty m where
   -- Relative Difficulty: 3
   -- (use banana and/or unicorn)
   furry' :: (a -> b) -> m a -> m b
-  furry' = error "todo"
- 
+  furry' f = banana $ unicorn . f
+
 -- Exercise 7
 -- Relative Difficulty: 2
 instance Misty [] where
-  banana = error "todo"
-  unicorn = error "todo"
- 
+  banana = concatMap
+  unicorn x = [x]
+
 -- Exercise 8
 -- Relative Difficulty: 2
 instance Misty Maybe where
-  banana = error "todo"
-  unicorn = error "todo"
- 
+  banana f (Just x) = f x
+  banana _ Nothing = Nothing
+  unicorn = Just
+
 -- Exercise 9
 -- Relative Difficulty: 6
 instance Misty ((->) t) where
-  banana = error "todo"
-  unicorn = error "todo"
- 
+  banana f a t = f (a t) t
+  unicorn = const
+
 -- Exercise 10
 -- Relative Difficulty: 6
 instance Misty (EitherLeft t) where
-  banana = error "todo"
-  unicorn = error "todo"
- 
+  banana f (EitherLeft (Left x)) = f x
+  banana _ (EitherLeft (Right x)) = EitherLeft $ Right x
+  unicorn = EitherLeft . Left
+
 -- Exercise 11
 -- Relative Difficulty: 6
 instance Misty (EitherRight t) where
-  banana = error "todo"
-  unicorn = error "todo"
- 
+  banana f (EitherRight (Right x)) = f x
+  banana _ (EitherRight (Left x)) = EitherRight $ Left x
+  unicorn = EitherRight . Right
+
 -- Exercise 12
 -- Relative Difficulty: 3
 jellybean :: (Misty m) => m (m a) -> m a
-jellybean = error "todo"
- 
+jellybean = banana id
+
 -- Exercise 13
 -- Relative Difficulty: 6
 apple :: (Misty m) => m a -> m (a -> b) -> m b
-apple = error "todo"
- 
+apple ma mf = banana (\a -> banana (unicorn . ($ a)) mf) ma
+
 -- Exercise 14
 -- Relative Difficulty: 6
 moppy :: (Misty m) => [a] -> (a -> m b) -> m [b]
-moppy = error "todo"
- 
+moppy [] _ = unicorn []
+moppy (x:xs) f = banana (\x' -> banana (\xs' -> unicorn $ x':xs') (moppy xs f)) (f x)
+
 -- Exercise 15
 -- Relative Difficulty: 6
 -- (bonus: use moppy)
 sausage :: (Misty m) => [m a] -> m [a]
-sausage = error "todo"
- 
+sausage = flip moppy id
+
 -- Exercise 16
 -- Relative Difficulty: 6
 -- (bonus: use apple + furry')
 banana2 :: (Misty m) => (a -> b -> c) -> m a -> m b -> m c
-banana2 = error "todo"
- 
+banana2 f a b = apple b $ furry' f a
+
 -- Exercise 17
 -- Relative Difficulty: 6
 -- (bonus: use apple + banana2)
 banana3 :: (Misty m) => (a -> b -> c -> d) -> m a -> m b -> m c -> m d
-banana3 = error "todo"
- 
+banana3 f a b c = apple c $ banana2 f a b
+
 -- Exercise 18
 -- Relative Difficulty: 6
 -- (bonus: use apple + banana3)
 banana4 :: (Misty m) => (a -> b -> c -> d -> e) -> m a -> m b -> m c -> m d -> m e
-banana4 = error "todo"
- 
-newtype State s a = State {
-  state :: (s -> (s, a))
-}
- 
+banana4 f a b c d = apple d $ banana3 f a b c
+
+newtype State s a = State { state :: s -> (s, a) }
+
 -- Exercise 19
 -- Relative Difficulty: 9
 instance Fluffy (State s) where
-  furry = error "todo"
- 
+  furry f (State s) = State $ \x -> let (x', a) = s x in (x', f a)
+
 -- Exercise 20
 -- Relative Difficulty: 10
 instance Misty (State s) where
-  banana = error "todo"
-  unicorn = error "todo"
+  banana f (State s) = State $ \x -> let (x', a) = s x in let (State s') = f a in s' x'
+  unicorn a = State $ \s -> (s, a)
+
